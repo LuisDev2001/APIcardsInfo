3325744a02dc4f2a535d257e998a64ab43fff5c6
LuiDev2001 <luisqrja@gmail.com>
2021-03-14 20:28:47 -0500

    Edit comment

--- router/cards.js
+++ router/cards.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-// Metodo "get" para listar productos
+// Metodo "get" para la informacion de cards
 router.get("/cards", (request, response, next) => {
   const json_cards = fs.readFileSync("db.json", "utf-8");
   const cards = JSON.parse(json_cards);
