fa4ff2500663d50919d452d10352231be8e58ee7
LuiDev2001 <luisqrja@gmail.com>
2021-03-14 20:42:58 -0500

    Add cors for http request

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require("express");
-
+const cors = require("cors");
 // APP
 const app = express();
 const PORT = process.env.PORT || 8000;
 // Get products list router api
 const cardsAPI = require("./router/cards.js");
 
+app.use(cors());
 // MANDANDO UN MENSAJE PARA QUE ES MUESTRE EN PANTALLA
 app.get("/", (request, response) => {
   response.send("Hola cons Express JS");
 });
 // Api get products list
-app.get("/cards", cardsAPI);
+app.get("/cards", cors(), cardsAPI);
 
 // INICIANDO EL SERVIDOR
 const server = app.listen(PORT, () => {
